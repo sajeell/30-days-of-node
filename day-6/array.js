a3ae426a7d5bdb3a6178721425c9dd90a4578efa
Sajeel Ahmad <sajeel.ahmed@protonmail.com>
2022-10-11 12:12:37 +0500

    feat(day-6-array): learning array methods

---
+++ day-6/array.js
@@ -0,0 +1,26 @@
+const weekdays = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+// Array Pop Method <- Removes the last element
+weekdays.pop();
+
+// Array Push Method <- Adds an element in the last
+weekdays.push('Saturday');
+
+// Array Unshift Method <- Adds an element in beginning
+weekdays.unshift('Sunday');
+
+// Array Shift Method <- Removes an element from the beginnning
+weekdays.shift('Sunday');
+
+// Array Reverse Method <- Reverses the array
+weekdays.reverse();
+
+// Array Sort Method <- Sorts the array
+weekdays.sort();
