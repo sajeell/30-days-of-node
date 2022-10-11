b660df07adddb227742066649390293531c29f8d
Sajeel Ahmad <sajeel.ahmed@protonmail.com>
2022-10-11 12:50:42 +0500

    feat!: done day-6 array methods

--- day-6/array.js
+++ day-6/array.js
@@ -24,3 +24,43 @@ weekdays.reverse();
 
 // Array Sort Method <- Sorts the array
 weekdays.sort();
+
+// Splice Method <- Adds element in the array
+weekdays.splice(2, 2, 'Wednesday', 'Thursday');
+
+// Splice Method <- Removes element in the array
+weekdays.splice(2, 2);
+
+// Concat Method <- Joins the array. Second array after the first array
+weekdays.concat(['Wednesday', 'Thursday']);
+
+// IndexOf Method <- Returns the index of the existing element, otherwise -1 if no matching element found
+weekdays.indexOf('Monday'); // Returns the index of Monday
+weekdays.indexOf('January'); // Returns -1, as there is no January element
+
+// LastIndexOf Method <- Returns the last index of the element
+const duplicatedElementsArray = [1, 2, 3, 4, 5, 5, 1, 5];
+duplicatedElementsArray.lastIndexOf(5); // Returns 7 as the index of the last 5 is 7
+
+// Join Method <- Joins the elements into a string, and returns a single string
+console.log(weekdays.join()); // Default seperator is ,
+console.log(weekdays.join(':')); // Now : is the seperator
+
+// Slice Method <- Extracts a section of the array, and returns a new array
+console.log(weekdays.slice(0, 1)); // Returns first element
+console.log(weekdays.slice(-2, -1)); // Starts from the last
+
+// Every Method <- Checks whether a provided condition is fulfilled by all the elements or not
+function checkType(value) {
+  return typeof value === 'string';
+}
+
+console.log(weekdays.every(checkType));
+
+// Reduce method
+
+function multiply(value, total) {
+  return value * total;
+}
+console.log(duplicatedElementsArray.reduce(multiply));
+
